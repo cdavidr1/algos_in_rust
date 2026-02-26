900809ea11a8c23d9dcf3cd583a545c817a8f27d
cdavidr1 <123332084+cdavidr1@users.noreply.github.com>
2026-02-26 15:17:53 -0500

    borr

--- src/subprograms/2_borrow.rs
+++ src/subprograms/2_borrow.rs
@@ -12,3 +12,9 @@ fn _abc(a: &mut Vec<i32>) {
 fn borrow_abc(i: &Vec<i32>) {
     println!("{}", i.len());
 }
+
+// swaps the first and last elements without taking ownership or cloning the strings
+fn swap_first_last(v: &mut Vec<String>) {
+    let last_index = v.len() - 1;
+    v.swap(0, last_index);
+}
