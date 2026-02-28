4975b98c2af4aeb0b3af5c27326cfb4269c7e2b1
cdavidr1 <123332084+cdavidr1@users.noreply.github.com>
2026-02-28 11:57:36 -0500

    lsp support + test swap

--- src/subprograms/2_borrow.rs
+++ src/subprograms/2_borrow.rs
@@ -1,6 +1,13 @@
 fn main() {
     let mut v = Vec::new();
     _abc(&mut v);
+
+    let mut swap_test = vec![
+        "first".to_string(),
+        "second".to_string(),
+        "third".to_string(),
+    ];
+    println!("{:?}", swap_first_last(&mut swap_test));
 }
 
 fn _abc(a: &mut Vec<i32>) {
@@ -14,7 +21,8 @@ fn borrow_abc(i: &Vec<i32>) {
 }
 
 // swaps the first and last elements without taking ownership or cloning the strings
-fn swap_first_last(v: &mut Vec<String>) {
+fn swap_first_last(v: &mut Vec<String>) -> &Vec<String> {
     let last_index = v.len() - 1;
     v.swap(0, last_index);
+    v
 }
