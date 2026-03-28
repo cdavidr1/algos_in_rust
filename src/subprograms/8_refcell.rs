50e054adfcd9b0a054757845f396d5926c2df61a
cdavidr1 <123332084+cdavidr1@users.noreply.github.com>
2026-03-28 13:39:23 -0400

    more refcell

--- src/subprograms/8_refcell.rs
+++ src/subprograms/8_refcell.rs
@@ -1,4 +1,7 @@
-use std::cell::RefCell;
+use std::{
+    cell::{Ref, RefCell},
+    collections::HashMap,
+};
 
 fn main() {
     let counter = SharedCounter::new();
@@ -8,6 +11,20 @@ fn main() {
     c.increment();
     c.increment();
     println!("Cur: {}", c.get());
+
+    let m = MutableMap::new();
+    m.insert("apple".to_string(), 5);
+    m.insert("orange".to_string(), 6);
+
+    if let Some(val) = m.get("apple") {
+        assert_eq!(*val, 5);
+    };
+
+    // without if let, guard is not dropped
+    // runtime panic: RefCell already borrowed
+    // let val = m.get("apple").unwrap(); // ← immutable borrow acquired
+    // assert_eq!(*val, 5);
+    m.insert("pear".to_string(), 4);
 }
 
 struct SharedCounter {
@@ -25,5 +42,22 @@ impl SharedCounter {
     }
     fn get(&self) -> usize {
         *self.count.borrow()
+    }
+}
+
+struct MutableMap {
+    map: RefCell<HashMap<String, i32>>,
+}
+impl MutableMap {
+    fn new() -> Self {
+        MutableMap {
+            map: RefCell::new(HashMap::new()),
+        }
+    }
+    fn insert(&self, key: String, val: i32) {
+        self.map.borrow_mut().insert(key, val);
+    }
+    fn get(&self, key: &str) -> Option<Ref<'_, i32>> {
+        Ref::filter_map(self.map.borrow(), |map| map.get(key)).ok()
     }
 }
